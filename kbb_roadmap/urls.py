101b4c40e072f48d171ed5749e27353409be9332
Gshopping <zays.kim@gmail.com>
2024-07-15 22:46:16 -0700

    add landing page and history

--- kbb_roadmap/urls.py
+++ kbb_roadmap/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('landing.urls')),
+    path('history/', include('history.urls')),
+
     path('admin/', admin.site.urls),
 
     path('user/', include('user.urls')),
