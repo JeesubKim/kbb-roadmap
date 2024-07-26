6d0b198ab60f104be824e66631463ec037a911fc
Gshopping <zays.kim@gmail.com>
2024-07-26 00:22:55 -0700

    add roadmap create

--- roadmap/models.py
+++ roadmap/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from user import models as user_model
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class TimestamedModel(models.Model):
@@ -16,23 +16,24 @@ class Roadmap(TimestamedModel):
     
     roadmap_name = models.CharField(blank=False, max_length=100)
     status = models.CharField(blank=False, max_length=50)
-    requester = models.ForeignKey(user_model.User, null=False, on_delete=models.CASCADE, related_name="roadmap_requester")
-    assignee = models.ForeignKey(user_model.User, null=False, on_delete=models.CASCADE, related_name="roadmap_assignee")
+    
+    requester = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE, related_name="roadmap_requester")
+    assignee = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE, related_name="roadmap_assignee")
     
 
 class RoadmapAgreement(TimestamedModel):
     roadmap_id = models.ForeignKey(Roadmap, null=False, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(user_model.User, null=False, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE)
 
 
 class RoadmapComplete(TimestamedModel):
     roadmap_id = models.ForeignKey(Roadmap, null=False, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(user_model.User, null=False, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE)
 
 
 class RoadmapComments(TimestamedModel):
-    author = models.ForeignKey(user_model.User, null=False, on_delete=models.CASCADE, related_name="roadmap_comment_author")
+    author = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE, related_name="roadmap_comment_author")
     roadmap = models.ForeignKey(Roadmap, null=False, on_delete=models.CASCADE, related_name="roadmap")
 
     contents = models.TextField(blank=True)
-    likes = models.ManyToManyField(user_model.User, related_name='comment_likes')
+    likes = models.ManyToManyField(get_user_model(), related_name='comment_likes')
--- roadmap/views.py
+++ roadmap/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from user.models import User as user_model
+from django.contrib.auth import get_user_model
 from . import models
 from .forms import RoadmapNameForm
 # Create your views here.
@@ -32,15 +33,16 @@ def new_roadmap(request):
             roadmap_name = form.cleaned_data["roadmap_name"]
             assignee = form.cleaned_data["assignee"]
             print(roadmap_name, assignee)
-            user = get_object_or_404(user_model, pk=request.user.id)
-
+            #user = get_object_or_404(user_model, pk=request.user.id)
+            user = get_object_or_404(get_user_model(), pk=request.user.id)
+            print('user', user)
 
             print(user)
             new_roadmap = models.Roadmap.objects.create(
                 roadmap_name = roadmap_name,
                 status = "Pending",
                 requester = user,
-                assignee = assignee,
+                assignee = user,
             )
             new_roadmap.save()
         return render(request, 'roadmap/roadmap.html')
